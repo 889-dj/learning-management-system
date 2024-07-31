b410cc214941241370b1c860025ef61e46b27569
Dev Jain <73821023+889-dj@users.noreply.github.com>
2024-07-31 11:20:52 +0000

    added swaggerui

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ export const app = express()
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import {ErrorMiddleware} from './middleware/error'
+
 // body parser
 app.use(express.json({limit:"50mb"}))
 
